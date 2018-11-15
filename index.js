139537f569c94731a77c656a26412e51c3509b5e
Alessandro Gatti <ale.gatti96@gmail.com>
2018-11-15 18:18:15 +0100

    remove unused utils.js

--- index.js
+++ index.js
@@ -3,4 +3,3 @@
 module.exports.MessagesManager = require('./src/messagesManager.js');
 module.exports.MessageBuffer = require('./src/buffer.js');
 module.exports.Message = require('./src/message.js');
-module.exports.utils = require('./src/utils.js');
